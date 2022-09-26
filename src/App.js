219382dcdd9205886b1b20cc4b6cc9bb645f7272
nicnic31 <permejonics@gmail.com>
2022-09-26 21:35:18 +0800

    Configured

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={window.location.pathname || ""}>
+      <BrowserRouter basename="https://nicnic31.github.io/productPreviewCard/">
         <Routes>
           <Route path="/productPreviewCard" element={<ProductPreview />} />
         </Routes>
