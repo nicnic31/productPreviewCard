667df0336a606729d9707f00414a6cb3c67c2a61
nicnic31 <permejonics@gmail.com>
2022-09-26 20:04:23 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ProductPreview from "./components/ProductPreview";
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename={window.location.pathname || ""}>
-        <Route exact path="/productPreviewCard" component={ProductPreview} />
+        <Routes>
+          <Route exact path="/" element={<ProductPreview />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
